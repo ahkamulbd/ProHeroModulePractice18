d4ce29eab52619ac6f96a7927844af3b452e00ec
ahkamulbd <ahkamulbd@gmail.com>
2022-08-02 16:09:44 +0600

    break & continue in JS seen according to Lectue 18-6

---
+++ break.js
@@ -0,0 +1,45 @@
+// // Ex 1
+
+// for (var i = 1; i < 20; i++) {
+//     console.log(i);
+//     if (i > 5) {
+//         break;
+//     }
+// }
+
+// // Ex 2
+
+// var roastGiven = 0;
+
+// while (roastGiven < 10) {
+//     console.log('Roast den, please !!!');
+//     roastGiven++;
+//     if (roastGiven > 4) {
+//         break;
+//     }
+// }
+
+// // Ex. 3
+
+// var items = ['bottle', 'mouse', 'sunglass', 'pen', 'notebook'];
+
+// for (var i = 0; i < items.length; i++) {
+//     var item = items[i];
+//     if (item == 'pen') {
+//         break;
+//     }
+//     console.log(item);
+// }
+
+// Ex. 4
+
+var numbers = [45, 87, 89, 56, 32, 51, 25, 188, 41, 25, 98];
+
+for (var i = 0; i < numbers.length; i++) {
+    var number = numbers[i];
+    if (number > 100) {
+        break;
+    }
+    console.log(number);
+}
+
