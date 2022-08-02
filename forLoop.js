e780e88b51ef76a2b7e71af40fd2045380a8bdd6
ahkamulbd <ahkamulbd@gmail.com>
2022-08-02 11:17:42 +0600

    for loop addressed according to Lecture 18-4

---
+++ forLoop.js
@@ -0,0 +1,25 @@
+// // Using for loop for roastGiven variable: 
+
+// for (var roastGiven = 0; roastGiven < 7; roastGiven++) {
+//     console.log(roastGiven);
+//     console.log('Roast Den, please !!!!!!!!!!!!');
+// }
+
+// // for loop Simple version:
+
+// for (var i = 0; i < 7; i++) {
+//     console.log(i);
+// }
+
+
+// // for loop Simple version to print odd number:
+
+// for (var i = 1; i <= 10; i += 2) {
+//     console.log(i);
+// }
+
+// for loop Simple version to print even number:
+
+for (var i = 0; i <= 10; i += 2) {
+    console.log(i);
+}
