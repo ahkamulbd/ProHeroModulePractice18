936ba1c64a1dcbb9a471bf3e00b40a6306271743
ahkamulbd <ahkamulbd@gmail.com>
2022-08-02 16:55:01 +0600

    reverse way in loop shown according to Lecture 18-7

--- break.js
+++ break.js
@@ -33,13 +33,14 @@
 
 // Ex. 4
 
-var numbers = [45, 87, 89, 56, 32, 51, 25, 188, 41, 25, 98];
+// var numbers = [45, 87, 89, 56, 32, 51, 25, 188, 41, 25, 98];
 
-for (var i = 0; i < numbers.length; i++) {
-    var number = numbers[i];
-    if (number > 100) {
-        break;
-    }
-    console.log(number);
-}
+// for (var i = 0; i < numbers.length; i++) {
+//     var number = numbers[i];
+//     if (number > 100) {
+//         break;
+//     }
+//     console.log(number);
+// }
+
 
---
+++ reverse.js
@@ -0,0 +1,14 @@
+// // Ex. 1
+
+// var num = 10;
+
+// while (num >= 1) {
+//     console.log(num);
+//     num--;
+// }
+
+// Ex. 2
+
+for (var i = 10; i >= 1; i--) {
+    console.log(i);
+}
