16f1861a400175731ec9860a1f88aa19b71ba224
ahkamulbd <ahkamulbd@gmail.com>
2022-08-02 06:52:50 +0600

    while.js created to initiate to explore loop

---
+++ while.js
@@ -0,0 +1,11 @@
+var roastGiven = 0;
+
+while (roastGiven < 7) {
+    console.log(roastGiven);
+    console.log('Roast Den, please !!!!!');
+    //roastGiven = roastGiven + 1;
+    //roastGiven += 1;
+    roastGiven++;
+    //console.log(roastGiven);
+}
+//console.log(roastGiven);
