ef7dd2c044d96470f88d0779efb251a3df5ad8aa
ahkamulbd <ahkamulbd@gmail.com>
2022-08-02 15:28:01 +0600

    How to print array in loop shown according to Lecture 18-5

---
+++ loopArray.js
@@ -0,0 +1,17 @@
+// Printing Array using loop:
+
+// var numbers = [45, 87, 89, 32, 51, 25];
+
+// for (var i = 0; i < numbers.length; numbers++) {
+//     var number = numbers[i];
+//     console.log(number);
+// }
+
+// Printing Array of String using loop:
+
+var items = ['bottle', 'mouse', 'sunglass', 'pen', 'mouse', 'keyboard', 'pen-stand'];
+
+for (var i = 0; i < items.length; i++) {
+    var item = items[i];
+    console.log(item);
+}
