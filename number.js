58147466f65ce70384b09e14b04028186c175d58
ahkamulbd <ahkamulbd@gmail.com>
2022-08-02 10:47:02 +0600

    Programme to print odd & even number using loop according to Lecture 18-3

---
+++ number.js
@@ -0,0 +1,23 @@
+// Using while loop to print 1 to 20
+
+// var number = 1;
+// while (number <= 20) {
+//     console.log(number);
+//     number++;
+// }
+
+// // Using while loop to print Odd Numbers from 1 to 20
+
+// var number = 1;
+// while (number <= 20) {
+//     console.log(number);
+//     number += 2;
+// }
+
+// Using while loop to print Odd Numbers from 1 to 20
+
+var number = 0;
+while (number <= 20) {
+    console.log(number);
+    number += 2;
+}
